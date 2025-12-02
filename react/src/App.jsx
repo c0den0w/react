0db4eb36f2ba0d6b65e36cc73415de410a0bc73e
bhavanishankar <bhavanishankar@pearl.bs>
2025-12-02 23:14:31 +0530

    started learning to add interactivity to react components using useState

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,43 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
 import BasicButtons from "./Button";
-import PackingList from "./list";
 import "./App.css";
-
 function App() {
-  const people = [
-    {
-      id: 0,
-      name: "Creola Katherine Johnson",
-      profession: "mathematician",
-    },
-    {
-      id: 1,
-      name: "Mario José Molina-Pasquel Henríquez",
-      profession: "chemist",
-    },
-    {
-      id: 2,
-      name: "Mohammad Abdus Salam",
-      profession: "physicist",
-    },
-    {
-      id: 3,
-      name: "Percy Lavon Julian",
-      profession: "chemist",
-    },
-    {
-      id: 4,
-      name: "Subrahmanyan Chandrasekhar",
-      profession: "astrophysicist",
-    },
-  ];
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const listitems = chemists.map((person) => (
-    <li key={person.id}>{person.name}</li>
-  ));
-  return <ul>{listitems}</ul>;
+  return (
+    <>
+      <BasicButtons
+        buttonOne={"First"}
+        buttonTwo={"Second"}
+        buttonThree={"Third"}
+      ></BasicButtons>
+    </>
+  );
 }
 
-export default App
+export default App;
--- react/src/Button.jsx
+++ react/src/Button.jsx
@@ -1,10 +1,18 @@
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { useState } from "react";
 
-export default function BasicButtons({buttonOne, buttonTwo, buttonThree}) {
+export default function BasicButtons({ buttonOne, buttonTwo, buttonThree }) {
+  let [index, setIndex] = useState(1);
+  const limit = 19;
+  function handleClick() {
+    if (index <= limit) setIndex(index + 1);
+  }
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="text">{buttonOne}</Button>
+      <Button variant="text" onClick={handleClick}>
+        {buttonOne + " " + index}
+      </Button>
       <Button variant="contained">{buttonTwo}</Button>
       <Button variant="outlined">{buttonThree}</Button>
     </Stack>
